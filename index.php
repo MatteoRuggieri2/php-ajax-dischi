6bb5e4c36f24d4438b17b72db4bda12f5e6eace9
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2022-01-26 15:30:59 +0100

    Milest.1 - Cartella img, header

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<?php echo 'test' ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-ajax-dischi</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <!-- HEADER -->
+    <header>
+        <div class="logo-container">
+            <img src="img/spotify-logo.png" alt="Spotify Logo">
+        </div>
+    </header>
+</body>
+</html>
