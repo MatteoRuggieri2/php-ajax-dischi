e06958d33d4651c4e24369b2eb590b494bb76cfd
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2022-01-26 17:10:27 +0100

    Milest.2 - Inserisco i dati dell'API in un array

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-alert('test')
+Vue.config.devtools = true;
+
+const app = new Vue({
+    el: '#root',
+    data: {
+        albumsArray: []
+    },
+    methods: {
+        getAlbumsApi: function() {
+            axios.get('http://localhost:8888/php-ajax-dischi/server.php')
+            .then((response) => {
+                this.albumsArray = response.data
+                console.log('mio array', this.albumsArray)
+            });
+        }
+    },
+    created: function() {
+        this.getAlbumsApi();
+    }
+})
